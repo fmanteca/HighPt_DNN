a9698ce7287c4b009213eda1d0f3dbb05b7f78ab
fmanteca <fmanteca@ifca.unican.es>
2020-05-11 13:22:40 +0200

    fixed step2

--- processing/step2.py
+++ processing/step2.py
@@ -6,26 +6,29 @@ all_files = glob.glob("/gpfs/users/mantecap/CMSSW_9_4_4/src/MyAnalysis/DNN/proce
 hits = []
 
 for filename in all_files:
-    df = pd.read_csv(filename, sep="\t", header=True)
+    df = pd.read_csv(filename)
     hits.append(df)
 
 frame_hits = pd.concat(hits, ignore_index=True)
 
+
 #TEST: 
-frame_hits.to_csv('test.csv', index=False)
+#frame_hits.to_csv('test.csv', index=False)
 
-# muon_files = glob.glob("/gpfs/users/mantecap/CMSSW_9_4_4/src/MyAnalysis/DNN/processing/data/output_Muon_*.txt")
+muon_files = glob.glob("/gpfs/users/mantecap/CMSSW_9_4_4/src/MyAnalysis/DNN/processing/data/output_Muon_*.txt")
 
-# muons = []
+muons = []
 
-# for filename in muon_files:
-#     df = pd.read_csv(filename, sep="\t", header=None)
-#     muons.append(df)
+for filename in muon_files:
+    df = pd.read_csv(filename)
+    muons.append(df)
 
-# frame_muons = pd.concat(muons, ignore_index=True)
-# frame_muons.columns = ['Muon_Eventid', 'Muon_Muonid', 'Muon_nHits', 'Muon_nGeomDets', 'Muon_Genpt', 'Muon_InnerTrack_pt', 'Muon_InnerTrack_eta', 'Muon_InnerTrack_phi', 'Muon_InnerTrack_charge', 'Muon_InnerTrack_ptErr', 'Muon_InnerTrack_Chindf', 'Muon_TunePTrack_pt', 'Muon_TunePTrack_ptErr']
+frame_muons = pd.concat(muons, ignore_index=True)
+
+#TEST: 
+#frame_muons.to_csv('test.csv', index=False)
 
 
-# output = pd.merge(frame_muons, frame_hits, on=['Muon_Eventid','Muon_Muonid'])
-# output.to_csv('TrainFile.csv', index=False)
+output = pd.merge(frame_muons, frame_hits, on=['Muon_Eventid','Muon_Muonid'])
+output.to_csv('TrainFile.csv', index=False)
 
